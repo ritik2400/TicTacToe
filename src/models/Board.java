e0ebc407adc537d073491ef38b29b3cc152a8fb7
Ritik Chawre <ritik1chawre@gmail.com>
2024-12-22 18:37:04 +0530

    Add cell methods in Board class.

--- src/models/Board.java
+++ src/models/Board.java
@@ -2,33 +2,37 @@ package models;
 
 public class Board {
 
-    char[][] board;
+    private char[][] cells;
 
     public Board() {
-        board = new char[3][3];
+        cells = new char[3][3];
     }
 
     public Board initBoard() {
         for(int i=0;i<3;i++) {
             for(int j=0;j<3;j++) {
-                board[i][j] = (char)('0' + (i + j));
+                cells[i][j] = (char)('0' + (i + j));
             }
         }
         return this;
     }
 
     public Board markCell(int x, int y, char symbol) {
-        this.board[x][y] = symbol;
+        this.cells[x][y] = symbol;
         return this;
+    }
+
+    public char getCell(int x, int y) {
+        return cells[x][y];
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("Board \n");
         for(int i=0;i<3;i++) {
-            sb.append("|| ").append(board[i][0])
-                    .append(" | ").append(board[i][1])
-                    .append(" | ").append(board[i][2])
+            sb.append("|| ").append(cells[i][0])
+                    .append(" | ").append(cells[i][1])
+                    .append(" | ").append(cells[i][2])
                     .append(" ||\n");
         }
         return sb.toString();
